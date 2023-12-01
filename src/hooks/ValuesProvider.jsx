5db5f9833b7b516560e856e44a89405fb6fb8c7a
Gabriel Gomes Silva <ggsilva.eng@gmail.com>
2023-12-01 15:10:18 -0300

    feat: histórico de buscas

--- src/hooks/ValuesProvider.jsx
+++ src/hooks/ValuesProvider.jsx
@@ -5,11 +5,22 @@ export default function ValuesProvider() {
   const [data, setData] = useState([]);
   const [search, setSearch] = useState(false);
   const [invalidCode, setInvalidCode] = useState(false);
-  const [ apiIsDown, setApiIsDown,] = useState(false);
+  const [apiIsDown, setApiIsDown,] = useState(false);
   const [loading, setLoading] = useState(false);
   const [clearPreviousSearch, setClearPreviousSearch] = useState(false);
+  const [emptyHistory, setEmptyHistory] = useState(true);
+  const [successfulSearchHistory, setSuccessfulSearchHistory] = useState([]);
+
 
   return ({
-    data, setData, search, setSearch, invalidCode, setInvalidCode,loading, setLoading,clearPreviousSearch, setClearPreviousSearch, apiIsDown, setApiIsDown,
+    data, setData,
+    search, setSearch,
+    invalidCode, setInvalidCode,
+    loading, setLoading,
+    clearPreviousSearch, setClearPreviousSearch,
+    apiIsDown, setApiIsDown,
+    successfulSearchHistory, setSuccessfulSearchHistory,
+    emptyHistory, setEmptyHistory
+
   });
 }
--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,3 +1,4 @@
+import  { useEffect } from 'react';
 import './styles.scss';
 import TrackingResult from './components/TrackingResult';
 import useUser from '../../hooks/useUser';
@@ -11,41 +12,61 @@ function App() {
     setSearch,
     invalidCode,
     setInvalidCode,
-    apiIsDown, setApiIsDown,
+    apiIsDown,
+    setApiIsDown,
     loading,
     setLoading,
     clearPreviousSearch,
-    setClearPreviousSearch
+    setClearPreviousSearch,
+    successfulSearchHistory,
+    setSuccessfulSearchHistory,
+    emptyHistory,
+    setEmptyHistory,
   } = useUser();
 
-  const InvalidCode = () => (
-    <>
-      <h2>Código de Rastreio inválido</h2>
-      <br />
-      <button className="att-button" onClick={handleNewSearch}>
-        <i className="fa fa-plus-circle"></i> Realizar outra pesquisa
-      </button>
-    </>
-  );
-  const APIDown = () => (
-    <>
-      <h2>API fora do ar...</h2>
-      <h3>Entre em contato com o Desenvolvedor</h3>
-      <br />
-      <button className="att-button" onClick={handleNewSearch}>
-        <i className="fa fa-plus-circle"></i> Realizar outra pesquisa
-      </button>
-    </>
-  );
-  const RastrearEncomenda = () => (
-    <form onSubmit={submitHandler} className="flex-center-column">
-      <h1>Rastrear Encomenda</h1>
-      <input type="text" className="form-control" name="tracking" />
-      <button type="submit" className="btn btn-primary">
-        Rastrear
-      </button>
-    </form>
-  );
+  const localStorageKey = 'searchHistory';
+
+  const updateSuccessfulSearchHistory = (trackingCode) => {
+    if (!successfulSearchHistory.includes(trackingCode)) {
+      setSuccessfulSearchHistory((prevHistory) => [...prevHistory, trackingCode]);
+      setEmptyHistory(false);
+      saveSearchHistoryToLocalStorage();
+    }
+  };
+  const setSelectedTrackingCode = async (trackingCode) => {
+    setSearch(true);
+    setInvalidCode(false);
+    setLoading(true);
+    setClearPreviousSearch(false);
+    setEmptyHistory(false);
+  
+    try {
+      const response = await axios.get(`/rastrear?tracking=${trackingCode}`);
+      const statusList = response.data.result.status_list;
+      setData(statusList);
+  
+      if (statusList.length === 0) {
+        setInvalidCode(true);
+      } 
+    } catch (error) {
+      setApiIsDown(true);
+      console.error("Erro ao acessar API", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  
+
+  const saveSearchHistoryToLocalStorage = () => {
+    localStorage.setItem(localStorageKey, JSON.stringify(successfulSearchHistory));
+  };
+
+  useEffect(() => {
+    const savedHistory = localStorage.getItem(localStorageKey);
+    if (savedHistory) {
+      setSuccessfulSearchHistory(JSON.parse(savedHistory));
+    }
+  }, [setSuccessfulSearchHistory]);
 
   const handleNewSearch = () => {
     setClearPreviousSearch(false);
@@ -64,8 +85,8 @@ function App() {
       setClearPreviousSearch(false);
     }
 
-    const formData = new FormData(event.target);
-    const data = Object.fromEntries(formData);
+    let formData = new FormData(event.target);
+    let data = Object.fromEntries(formData);
 
     try {
       const response = await axios.get(`/rastrear?tracking=${data.tracking}`);
@@ -74,30 +95,73 @@ function App() {
 
       if (statusList.length === 0) {
         setInvalidCode(true);
+      } else {
+        updateSuccessfulSearchHistory(data.tracking);
       }
     } catch (error) {
       setApiIsDown(true);
-
       console.error("Erro ao acessar API", error);
-
-      return [];
     } finally {
       setLoading(false);
     }
   };
 
+  const HistoricoDeBusca = () => (
+    <>
+      <h2>Histórico de Busca</h2>
+      <ul>
+        {successfulSearchHistory.map((trackingCode) => (
+          <li key={trackingCode} onClick={() => setSelectedTrackingCode(trackingCode)} style={{ cursor: 'pointer' }}>
+            {trackingCode}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+
+  const InvalidCode = () => (
+    <>
+      <h2>Código de Rastreio inválido</h2>
+      <br />
+      <button className="att-button" onClick={handleNewSearch}>
+        <i className="fa fa-plus-circle"></i> Realizar outra pesquisa
+      </button>
+    </>
+  );
+
+  const APIDown = () => (
+    <>
+      <h2>API fora do ar...</h2>
+      <h3>Entre em contato com o Desenvolvedor</h3>
+      <br />
+      <button className="att-button" onClick={handleNewSearch}>
+        <i className="fa fa-plus-circle"></i> Realizar outra pesquisa
+      </button>
+    </>
+  );
+
+  const RastrearEncomenda = () => (
+    <form onSubmit={submitHandler} className="flex-center-column">
+      <h1>Rastrear Encomenda</h1>
+      <input type="text" className="form-control" name="tracking" />
+      <button type="submit" className="btn btn-primary">
+        Rastrear
+      </button>
+    </form>
+  );
+
   return (
     <div className="container flex-center-column overlay">
       <div className="trackingContent flex-center-column">
         {loading && <h2>Carregando...</h2>}
-
         {!search && !loading && !clearPreviousSearch && <RastrearEncomenda />}
-
         {search && !loading && <TrackingResult data={data} setSearch={setSearch} setInvalidCode={setInvalidCode} />}
-
         {search && invalidCode && !loading && <InvalidCode />}
-
         {search && apiIsDown && !loading && <APIDown />}
+      </div>
+
+      <div className="trackingContent flex-center-column">
+        {!emptyHistory ? <HistoricoDeBusca /> : <h3>Histórico de busca está vazio</h3>}
       </div>
     </div>
   );
